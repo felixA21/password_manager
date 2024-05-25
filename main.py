4f822fcbcb873276090ca79ee1d1b456f1724b4d
Felix Alimin <felixalimin@gmail.com>
2024-05-25 19:25:11 -0400

    added save function to save password entries into a txt file

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"{website} | {email} | {password}\n")
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
+        website_entry.focus()
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -26,15 +36,17 @@ password_label.grid(row=3, column=0)
 # Entries
 website_entry = Entry(width=35)
 website_entry.grid(row=1, column=1, columnspan=2, sticky="EW")
+website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2, sticky="EW")
+email_entry.insert(0, "corgi@example.com")
 password_entry = Entry(width=25)
 password_entry.grid(row=3, column=1, sticky="EW")
 
 # Buttons
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(row=3, column=2, sticky="EW")
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, sticky="EW")
 
 
